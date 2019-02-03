248151a18b9511e5d9c69a5e96cf57ef97cb40d6
Sora Kicuchi <skdrums@Soras-MacBook-Air.local>
2019-02-03 19:11:44 +0900

    add new file

---
+++ 3_1_3sort.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+
+#define N 8
+
+int main(void){
+    int a[]={80,40,35,12,88,1,56,33};
+    int left,right,shift,t;
+    left=0;right=N-1;
+    while(left<right){
+        for(int i = left; i < right; i++)
+        {
+            if(a[i]>a[i+1]){
+                t=a[i];
+                a[i]=a[i+1];
+                a[i+1]=t;
+                shift=i;
+            }
+        }
+        right=shift;
+
+        for(int i = right; i > left; i--)
+        {
+            if(a[i]<a[i-1]){
+                t=a[i];a[i]=a[i-1];a[i-1]=t;
+                shift=i;
+            }
+        }
+        left=shift;
+        
+    }
+
+    for(int i = 0; i < N; i++)
+    {
+        cout << a[i] <<" ";
+    }
+    
+
+}
