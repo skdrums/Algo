9af79602821e3fc02c5ebbff9c29dcf750dd9467
Sora Kicuchi <skdrums@Soras-MacBook-Air.local>
2019-02-03 18:41:35 +0900

    add new file

---
+++ 3_1_1sort.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+
+#define N 8
+
+int main(void){
+    int a[]={80,40,35,12,88,1,56,33};
+    int t;
+
+     for(int i = 0; i < N-1; i++)
+     {
+        int min=a[i];
+        int s=i;
+        for(int j = i+1; j < N; j++)
+        {
+            if (a[j]<min) {
+                min=a[j];
+                s=j;
+            }
+            
+
+        }
+        t=a[i];
+        a[i]=min;
+        a[s]=t;
+     }
+     
+
+     for(int i = 0; i < N; i++)
+     {
+         cout<<a[i]<<" ";
+     }
+     
+    
+
+
+
+
+
+
+}
