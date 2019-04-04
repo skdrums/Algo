9029040bdae2c7869fbfc824d39990aeed40adb1
skdrums <sora.kannaduki1009@gmail.com>
2019-04-04 13:04:26 +0900

    edit file array.go

--- testForGo/array.go
+++ testForGo/array.go
@@ -5,11 +5,30 @@ import (
 
 func main(){
 	const N int = 5// constant定数
-	var arr1 = [...]int{4,5,5,8,9} // variable変数 
 	
+	//配列（固定長）
+	//var arr []int
+	var arr1 = [5]int{4,5,5,8,9} // variable変数 
+	var arr2 = [...]string{"sora","yoko","ryo","shoto","mei"}// ...で長さを暗黙的に指定
+
+	//slice (可変長)
+	//var slice []int
+	slice2 := []string{"sora","yoko","ryo","shoto","mei"}
+
+	fmt.Println("array..")
 	for i :=0; i<N; i++{
 		fmt.Println(arr1[i])
 	}
-
+	fmt.Print("\n")
+	fmt.Println("array..")
+	for i :=0; i<N; i++{
+		fmt.Println(arr2[i])
+	}
+	fmt.Print("\n")
+	fmt.Println("slice..")
+	for i :=0; i<N; i++{
+		fmt.Println(slice2[i])
+	}
+	fmt.Print("\n")
 
 }
